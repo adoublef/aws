bb1fa2fee220c108c27980bebfe651a414dd66eb
Rahim <kristopherab@gmail.com>
2023-06-25 13:06:13 +0100

    Fix Terraform (#8)
    * add ignore file
    
    * add makefile
    
    * tf init
    
    * add actions
    
    * update plan
    
    * update concurrency
    
    * update format
    
    * update format with check
    
    * revert fmt
    
    * remove econ-on-err
    
    * add tfstate resource
    
    * makefile update
    
    * fix module source
    
    * update tfstate module
    
    * add tokens
    
    * using s3 for backend
    
    * add eenv
    
    * move env inside job
    
    * add ecr module
    
    * update tf-apply
    
    * add ecr module
    
    * typo fix
    
    * change ecr name
    
    * add readme
    
    * add home service
    
    * add go-test
    
    * update go-test
    
    * only use latest version
    
    * fix deps install
    
    * add docker image
    
    * add global middleware
    
    * mod tidy
    
    * run app in dev mode
    
    * add script to push to ecr
    
    * ecr-push action
    
    * fix region error
    
    * use ready-made module
    
    * rename module for ecr
    
    * update to 1st party modules

--- infra/main.tf
+++ infra/main.tf
@@ -7,31 +7,83 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "adoublef-aws-tfstate"
-    key            = "infra.tfstate"
     region         = "eu-west-2"
+    key            = "adoublef.aws.tfstate"
+    bucket         = "adoublef-aws-tfstate"
     dynamodb_table = "adoublef-aws-tfstate-lock"
     encrypt        = true
   }
 }
 
-provider "aws" {
-  region = "eu-west-2"
+locals {
+  region           = "eu-west-2"
+  repository       = "adoublef-aws-repository"
+  tfstate_bucket   = "adoublef-aws-tfstate"
+  tfstate_dynamodb = "adoublef-aws-tfstate-lock"
 }
 
-module "tfstate" {
-  source = "./modules/aws/tfstate"
+provider "aws" {
+  region = local.region
+}
+
+module "tfstate_s3" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = local.tfstate_bucket
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
+
+module "tfstate_dynamodb" {
+  source = "terraform-aws-modules/dynamodb-table/aws"
+
+  name         = local.tfstate_dynamodb
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attributes = [
+    {
+      name = "LockID"
+      type = "S"
+    }
+  ]
+
+  tags = {
+    Terraform = "true"
+  }
 }
 
 module "ecr" {
   source          = "terraform-aws-modules/ecr/aws"
-  repository_name = "adoublef-aws-ecr"
+  repository_name = local.repository
 
-    # repository_read_write_access_arns
-    # repository_lifetime_policy
+  # repository_read_write_access_arns
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 30 images",
+        selection = {
+          tagStatus   = "any",
+          countType   = "imageCountMoreThan",
+          countNumber = 30
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 
-    tags = {
-        Terraform = "true"
-        Environment = "dev"
-    }
+  repository_force_delete = true
+
+  tags = {
+    Terraform = "true"
+  }
 }
--- infra/modules/aws/ecr/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecr_repository" "registry" {
-  name                 = var.ecr_name
-  image_tag_mutability = var.image_tag_mutability
-}
